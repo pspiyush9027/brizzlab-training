3e7386b870993e52f32639468929040140ea823a
Piyush <piyushagrawa9027@gmail.com>
2026-01-14 16:04:22 +0530

    [piyush] .add - basics of javascript

---
+++ jsintro.js
@@ -0,0 +1,45 @@
+/** 
+ * This is a JavaScript file.
+ */
+
+console.log("Hello, World!"); // This line prints a greeting message to the console.
+
+let name = "piyush";          //string
+let age = 21;                 //number
+const country = "India";      //constant
+let isStudent = true;         //boolean
+
+console.log( name);
+console.log( age);
+console.log( country);
+console.log( isStudent);
+
+//datatypes in js
+//string, number, boolean, null, undefined, object, symbol
+let score;         //undefined
+let data = null;   //null
+
+console.log( score);
+console.log( data);
+
+//object
+
+let a={name:"piyush", age:21, countrys:"India"}; //object 
+
+a.country={
+    state1:"Uttar Pradesh",
+    state2:"Bihar"
+}        //object inside object
+
+console.log( a.countrys);
+console.log( a.country.state1);
+
+//array
+
+let arr=[1,2,3,4,5]; //array of numbers
+let arr2=["piyush", "rahul", "sachin"]; //array of strings
+let arr3=[1, "piyush", true, null]; //array of mixed datatypes
+
+console.log( arr);
+console.log( arr2);
+console.log( arr3); 
